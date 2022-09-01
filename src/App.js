a5be2e907ab81cc4be5655fb5a1398cf1824fbde
CSCairney <charlescairney@yahoo.co.uk>
2022-09-01 21:58:51 +0100

    read more exercise added (expand text)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import game from './Game';
+import HideInfo from './HideInfo';
 
 function App() {
   return (
-    <>
-    <game />
-    </>
+    <HideInfo text={"According to the Republic of San Magnolia, their ongoing war against the Giadian Empire has no casualties—however, that is mere propaganda. While the silver-haired Alba of the Republic's eighty-five sectors live safely behind protective walls, those of different appearances are interned in a secret eighty-sixth faction. Known within the military as the Eighty-Six, they are forced to fight against the Empire's autonomous Legion under the command of the Republican Handlers. Vladilena Milizé is assigned to the Spearhead squadron to replace their previous Handler.Shunned by her peers for being a fellow Eighty - Six supporter, she continues to fight against their inhumane discrimination.Shinei Nouzen is the captain of the Spearhead squadron.Infamous for being the sole survivor of every squadron he's been in, he insists on shouldering the names and wishes of his fallen comrades. When the fates of these young souls from two different worlds collide, will it ignite the spark that lights their path to salvation, or will they burn themselves in the flames of despair?"}
+    />
   );
 }
 
